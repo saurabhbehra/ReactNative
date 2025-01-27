db115bacd2947b8c1656439137d6091f581acfbd
saurabhbehra <saurabhbehra5@gmail.com>
2025-01-27 18:05:49 +0530

    todo app

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,46 +1,12 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import { Appearance } from 'react-native';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { Colors } from '@/constants/Colors';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  // const colorScheme = useColorScheme();
-  const colorScheme = Appearance.getColorScheme()
-  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light;
-
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerStyle: { backgroundColor: theme.headerBackground }, headerTintColor: theme.text, headerShadowVisible: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="index" options={{ title:'Home', headerShown: false}} />
-        <Stack.Screen name="explore" options={{ title:'Contact'}} /> */}
-        <Stack.Screen name="+not-found" />
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
-      // <StatusBar style="auto" />
-    // </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
